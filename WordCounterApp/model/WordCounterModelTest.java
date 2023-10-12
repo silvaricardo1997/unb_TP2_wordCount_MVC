c45ead8a1fb7299fd611b61bb3cc82e4b8990763
artdelpi <123995888+artdelpi@users.noreply.github.com>
2023-10-12 09:36:21 -0300

    Add files via upload

---
+++ WordCounterApp/model/WordCounterModelTest.java
@@ -0,0 +1,102 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+class WordCounterModelTest {
+    WordCounterModel testModel = new WordCounterModel();
+    Map<String, Integer> testWordFrequency = new LinkedHashMap<>();
+
+    @Test
+    public void testCountWordsFromFile_IOExceptionOnFileNotFound() throws IOException {
+        // Verifica lançamento de exceção
+        assertThrows(IOException.class, () -> {
+            testModel.countWordsFromFile("/invalid.txt", "/invalid.txt", 0);
+        });
+    }
+
+    @Test
+    public void testCountWordsFromFile_OnlyNumbersAndSpecialCharacters() throws IOException {
+        // when
+        testWordFrequency = testModel.countWordsFromFile("src/test/java/model/UnitTestingFiles/test-numbers-n-schars.txt", "src/test/java/model/UnitTestingFiles/test-stop-words.txt", 1);
+
+        // then
+        assertTrue(testWordFrequency.isEmpty());
+    }
+
+    @Test
+    public void testCountWordsFromFile_OnlyStopWords() throws IOException {
+        // when
+        testWordFrequency = testModel.countWordsFromFile("src/test/java/model/UnitTestingFiles/test-stop-words.txt", "src/test/java/model/UnitTestingFiles/test-stop-words.txt", 1);
+
+        // then
+        assertTrue(testWordFrequency.isEmpty());
+    }
+
+    @Test
+    public void testCountWordsFromFile_TrivialWithStopWords() throws IOException {
+        // given
+        Map<String, Integer> testCorrectOutput = new LinkedHashMap<>() {{
+            put("degree", 6);
+            put("light", 4);
+            put("trivial", 2);
+            put("thus", 2);
+            put("concert", 2);
+            put("without", 2);
+        }};
+
+        // when
+        testWordFrequency = testModel.countWordsFromFile("src/test/java/model/UnitTestingFiles/test-texto.txt", "src/test/java/model/UnitTestingFiles/test-stop-words.txt", 30);
+
+        // then
+        Assertions.assertEquals(testWordFrequency, testCorrectOutput);
+    }
+
+    @Test
+    public void testCountTotalWordsExcludingStopwords_TrivialWithStopWords() throws IOException {
+        // given
+        int expected = 18;
+
+        // when
+        int actual = testModel.countTotalWordsExcludingStopwords("src/test/java/model/UnitTestingFiles/test-texto.txt", "src/test/java/model/UnitTestingFiles/test-stop-words.txt");
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCountTotalWordsExcludingStopwords_OnlyStopWords() throws IOException {
+        // given
+        int expected = 0;
+
+        // when
+        int actual = testModel.countTotalWordsExcludingStopwords("src/test/java/model/UnitTestingFiles/test-stop-words.txt", "src/test/java/model/UnitTestingFiles/test-stop-words.txt");
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCountTotalWordsExcludingStopwords_OnlySpecialCharacters() throws IOException {
+        // given
+        int expected = 0;
+
+        // when
+        int actual = testModel.countTotalWordsExcludingStopwords("src/test/java/model/UnitTestingFiles/test-numbers-n-schars.txt", "src/test/java/model/UnitTestingFiles/test-stop-words.txt");
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
